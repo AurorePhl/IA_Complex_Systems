fa852d70c557b3f1f7053f34e121437afb736c54
drtLaura <laura.duret.42@gmail.com>
2025-01-16 23:52:33 +0100

    récupération survivants

--- js/simulation.js
+++ js/simulation.js
@@ -7,18 +7,35 @@ function initGrid(city,taille) {
             city[i][j] = { 
                 fire: false, // au début, il n'y a pas de feu sur les cases
                 robots: [], // listes des robots possiblement sur la case, au début aucun robot
-                survivants: [], // liste des survivants possiblement sur la case, au début aucun survivant
-                qg : false // au début, il n'y a pas de QG sur les cases
+                survivant: false, // au début aucun survivant, on suppose qu'il peut y avoir un seul survivatn sur une cellule
+                qg : null // au début, il n'y a pas de QG sur les cases
             }; 
         }
     }
 
     // quatier général
     let mid = Math.floor(taille / 2);
-    city[mid][mid].qg = true;
+    city[mid][mid].qg = {
+        nbSurvivants: 0 
+    };
     // peut etre mettre 4 cases pour le QG pour avoir exactement le milieu mais on verra après
 
+    // les survivants
+    placeSurvivants(city, taille, 10);
+
     return city;
+}
+
+// placer des survivants aléatoirement dans la grille
+function placeSurvivants(city, taille, count) {
+    while (count > 0) {
+        let x = Math.floor(Math.random() * taille);
+        let y = Math.floor(Math.random() * taille);
+        if (!city[x][y].survivant && !city[x][y].qg) { // on ne peut pas placer un survivant sur un QG
+            city[x][y].survivant = true; // on place un survivant
+            count--;
+        }
+    }
 }
 
 // pour visualiser les robots sur les cellules
@@ -54,27 +71,56 @@ function updateGrid(city, taille, cellSize,robots) {
                 let positions = getRobotPositions(cellSize, cell.robots.length);
                 cell.robots.forEach((robot, index) => {
                     let pos = positions[index];
-                    svgContent += `<circle cx="${j * cellSize + pos.cx}" cy="${i * cellSize + pos.cy}" r="${cellSize / 8}" fill="blue" />`;
+                    svgContent += `<circle cx="${j * cellSize + pos.cx}" cy="${i * cellSize + pos.cy}" r="${cellSize / 8}" fill="${robot.color}" />`;
                 });
+            }
+            if (cell.survivant) { // afficher les survivants
+                let x = j * cellSize + cellSize / 2;
+                let y = i * cellSize + cellSize / 2;
+                let size = cellSize / 4;
+                svgContent += `<polygon points="${x},${y - size} ${x - size},${y + size} ${x + size},${y + size}" fill="green" />`;
+            }
+            if (cell.qg) { // afficher le nombre de survivants au QG (informations)
+                let x = j * cellSize + cellSize / 2;
+                let y = i * cellSize + cellSize / 2;
+                svgContent += `<text x="${x}" y="${y}" font-size="${cellSize / 4}" text-anchor="middle" fill="black">${cell.qg.nbSurvivants}</text>`;
+                let triangleSize = cellSize / 6; 
+                let triangleX = x + cellSize / 4; 
+                let triangleY = y - cellSize / 8;
+                svgContent += `<polygon points="${triangleX},${triangleY} ${triangleX - triangleSize},${triangleY + triangleSize} ${triangleX + triangleSize},${triangleY + triangleSize}" fill="green" />`;
             }
         }
     }
-
     return svgContent;
 }
 
  // Positionne 7 robots aléatoirement dans la grille
  function placeRobots(city,taille, count) {
     let robots = [];
+    let robotColor = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink'];
+
     let mid = Math.floor(taille / 2);
 
     for (let i = 0; i < count; i++) {
-        let robot = { x: mid, y: mid, id: i};
+        let color = robotColor[i % robotColor.length]; // couleur du robot
+        let robot = { x: mid, y: mid, id: i, color: color, hasSurvivor: false }; // caracteristiques du robot
+
         city[mid][mid].robots.push(robot); // ajouter à city (caracteristiques des cellules)
         robots.push(robot); // ajouter à la liste des robots > peut etre a enlever
+
+        setInterval(() => { // bouger les robots
+            moveRobot(city, robot, taille); // bouger le robot
+            const grid = document.getElementById('grid'); // mettre à jour la grille
+            let svgContent = updateGrid(city, taille, 50, robots);  // mettre à jour la grille
+            grid.innerHTML = svgContent; // mettre à jour la grille
+        }, 500); // bouger les robots toutes les secondes
+
     }
+    console.log('voici les robots', robots);
     return robots;
+    
 }
+
 
 // Allume un feu à une position donnée
 function startFire(city, x, y) {
@@ -85,6 +131,62 @@ function startFire(city, x, y) {
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getDirectionToQG(city, robot) { // direction du robot vers le QG
+    const mid = Math.floor(city.length / 2);
+    const dx = mid - robot.x;
+    const dy = mid - robot.y;
+
+    if (Math.abs(dx) > Math.abs(dy)) {
+        return { dx: Math.sign(dx), dy: 0 }; 
+    } else {
+        return { dx: 0, dy: Math.sign(dy) }; 
+    }
+}
+
+function sendInformation(city) { // pour le moment il y a juste les informations pour le nombre de survivants
+    let mid = Math.floor(city.length / 2); // recupérer le qg
+    if (city[mid][mid].qg) { // si le QG existe (vérification)
+        city[mid][mid].qg.nbSurvivants += 1; // ajouter un survivant au QG
+    }
+}
+
+function moveRobot(city, robot, taille) {
+    const directions = [
+        { dx: 0, dy: -1 }, // haut
+        { dx: 0, dy: 1 },  // bas
+        { dx: -1, dy: 0 }, // gauche
+        { dx: 1, dy: 0 }   // droite
+    ];
+
+    let direction; // direction du robot
+    if (robot.hasSurvivor) { // si le robot a un survivant
+        direction = getDirectionToQG(city, robot); // aller vers le QG
+    } else { // sinon
+        direction = directions[Math.floor(Math.random() * directions.length)]; // aller dans une direction aléatoire
+    }
+
+    const newX = robot.x + direction.dx; // nouvelle position du robot
+    const newY = robot.y + direction.dy; // nouvelle position du robot
+
+    if (newX >= 0 && newX < taille && newY >= 0 && newY < taille) { // si la nouvelle position est dans la grille
+        city[robot.x][robot.y].robots = city[robot.x][robot.y].robots.filter(r => r.id !== robot.id); // enlever le robot de la case actuelle
+
+        robot.x = newX;
+        robot.y = newY;
+
+        city[newX][newY].robots.push(robot); // ajouter le robot à la nouvelle case
+
+        if (robot.hasSurvivor && city[newX][newY].qg) { // si le robot a un survivant et qu'il est sur le QG
+            
+            robot.hasSurvivor = false;
+        } else if (!robot.hasSurvivor && city[newX][newY].survivant) { // si le robot n'a pas de survivant et qu'il y a un survivant sur la case
+            robot.hasSurvivor = true;
+            sendInformation(city); // envoyer les informations au QG (meme si le survivant n'est pas encore au QG)
+            city[newX][newY].survivant = false; // enlever le survivant de la case
+        }
+    }
 }
 
 // Appel au chargement de la page
